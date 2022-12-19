4ed7e7a746591d8c74e112d754b16046aa9d66ac
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-19 00:20:47 -0300

    Do exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  for (let item of args) {
+    if (arr.indexOf(item) < 0) {
+      continue;
+    } else {
+      arr.splice(arr.indexOf(item), 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
