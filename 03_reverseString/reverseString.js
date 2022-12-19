67799d29f9b17662b647743793b9cdeed05cf6d2
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-18 23:14:52 -0300

    Do exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let rts = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rts = rts + str[i];
+  }
+  return rts;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
