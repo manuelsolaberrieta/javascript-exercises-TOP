c1b2e1250dd358c77fb6193d1ecdb4455f9ac8f0
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-19 13:25:08 -0300

    Do exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (start, finish) {
+  let sum = 0;
+  if (start < 0 || finish < 0) {
+    return "ERROR";
+  }
+  if (typeof start !== "number" || typeof finish !== "number") {
+    return "ERROR";
+  }
+  if (finish < start) {
+    let aux = start;
+    start = finish;
+    finish = aux;
+  }
+  for (let i = start; i <= finish; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
