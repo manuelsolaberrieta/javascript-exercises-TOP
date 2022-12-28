602cc8ab7aa35c957a07177cead0c8c04b5233f1
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-28 20:10:17 -0300

    Do 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let rts = "";
+  str = str.replace(/[?.!, ]/g, "").toLowerCase();
+  for (let i = str.length - 1; i >= 0; i--) {
+    rts = rts + str[i];
+  }
+  if (rts == str) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
