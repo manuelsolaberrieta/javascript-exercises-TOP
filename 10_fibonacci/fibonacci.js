601812efb50bd58f5fa66437882fcf2189adfe22
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-28 23:52:55 -0300

    Do fiboFuckingNazi

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    let sequence = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence[num];
+  }
 };
 
 // Do not edit below this line
