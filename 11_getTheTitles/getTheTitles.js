00ddc59c01c25a4fb67c0bf7829cccda59f92a90
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-29 20:12:53 -0300

    Do 11th basic js exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  let titles = [];
+  obj.forEach((e) => {
+    titles.push(e.title);
+  });
+  return titles;
 };
 
 // Do not edit below this line
