94a1517a2bee98bff8dd7e12b018e749afd00305
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-30 00:00:41 -0300

    Do 12th basic javascript exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let oldest = arr[0];
+  function getAge(obj) {
+    if (obj.hasOwnProperty("yearOfDeath")) {
+      return obj.yearOfDeath - obj.yearOfBirth;
+    } else {
+      let currentYear = new Date().getFullYear();
+      return currentYear - obj.yearOfBirth;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (getAge(arr[i]) > getAge(oldest)) {
+      oldest = arr[i];
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
